fb49825022f33def0b9e5659e4a0fb839f34b9fd
Yunus <yunus@fastmail.com>
2019-01-03 18:10:51 +0300

    Commit

---
+++ 09_Регулярные выражения/ex_01.js
@@ -0,0 +1,7 @@
+console.log(/ca(r|t)/.test("cat"));
+console.log(/pr?op/.test("pop"));
+console.log(/ferr(et|y|ari)/.test("ferret"));
+console.log(/\b\w+ious\b/.test("conscious person"));
+console.log(/\s[.,:;]/.test(" ."));
+console.log(/\b\w{6,}\b/.test("capacitor"));
+console.log(/\b[^eE]+\b/.test("hullo"));
